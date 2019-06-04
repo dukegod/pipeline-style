ad66fdc71ff080549db00e95b5dffdb399cea82a
dukegod <dukehui1991@gmail.com>
2019-06-05 01:03:07 +0800

    eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,33 @@
 module.exports = {
-  "parser": "babel-eslint",
-    "extends": ['airbnb', "plugin:react/recommended", "prettier"],
-    "parserOptions": {
-        "ecmaVersion": 8,
-        "sourceType": "module",
-        "ecmaFeatures": {
-            "jsx": true,
-            "modules": true,
-            "experimentalObjectRestSpread": true
-        }
+  parser: 'babel-eslint',
+  extends: ['airbnb', 'plugin:react/recommended', 'prettier'],
+  parserOptions: {
+    ecmaVersion: 8,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+      experimentalObjectRestSpread: true,
     },
-    "plugins": ["react"],
-    "settings": {
-        "react": {
-            "pragma": "React",
-            "version": "16.1.1"
-        }
+  },
+  plugins: ['react'],
+  settings: {
+    react: {
+      pragma: 'React',
+      version: '16.1.1',
     },
-    "env": {
-        browser: true,
-        es6: true,
-        node: true,
-    },
+  },
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
 
-    "globals": {
-        __DEV__: true,
-        __HTTPS__: true,
-        $: true
-    },
+  globals: {
+    __DEV__: true,
+    __HTTPS__: true,
+    $: true,
+  },
 
-    "rules": {
-
-    }
-}
+  rules: {},
+};

f4c144999d706cc435b206be3d64fd2aaf031de9
dukegod <dukehui1991@gmail.com>
2019-06-05 22:10:44 +0800

    test

--- test/test.js
+++ test/test.js
@@ -1,15 +1,7 @@
-function name(params) {
+function name() {
   const NAME = 99;
   console.log(NAME);
 }
 
-let i = '90';
 
-var arr = [12, 34];
-
-let obj = {
-  x: 1,
-  y: 2,
-};
-
-var st = '90';
+      name();
